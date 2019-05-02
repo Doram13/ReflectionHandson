b87d6be12fe1fc23d4df8cd38d17fd8cb805413f
Doram13 <1991doram@gmail.com>
2019-05-02 14:25:39 +0200

    Initial commit

---
+++ Reflection Hands-on/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection_Hands_on
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowAssembly(new Assembly());
+            Console.ReadLine();
+        }
+
+        public static void ShowAssembly(Assembly assembly)
+        {
+
+            Type ClassType = assembly.GetType();
+            
+            Console.WriteLine(ClassType.AssemblyQualifiedName);
+            Console.WriteLine(ClassType.FullName);
+            Console.WriteLine(ClassType.Assembly.GlobalAssemblyCache);
+            Console.WriteLine(ClassType.Assembly.Location);
+            Console.WriteLine(ClassType.Assembly.ImageRuntimeVersion);
+
+        }
+    }
+}
